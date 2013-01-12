529d860f9bdbc1ae701d7c00bbf11b4c12b5d6dd
Vadim Martos <vadim.martos@gmail.com>
2013-01-13 00:23:29 +0300

    added validation for provider fields

--- ibs/ibs-common/src/main/java/edu/ibs/common/enums/ProviderField.java
+++ ibs/ibs-common/src/main/java/edu/ibs/common/enums/ProviderField.java
@@ -1,5 +1,7 @@
 package edu.ibs.common.enums;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @date Jan 12, 2013
@@ -11,22 +13,58 @@ public enum ProviderField {
 	PHONE("phone"),
 	NAME("name"),
 	ADDRESS("address");
+	private static final String PHONE_STRING_REGEXP = "^[375]{3}[0-9]{9}$";
+	private static final Pattern PHONE_STRING_PATTERN = Pattern.compile(PHONE_STRING_REGEXP);
+	private static final String NAME_STRING_REGEXP = "[а-яА-Я]{2,32}[-]{0,1}[а-яА-Я]{2,32}";
+	private static final Pattern NAME_STRING_PATTERN = Pattern.compile(NAME_STRING_REGEXP);
+	private static final String ADDRESS_STRING_REGEXP = "[\\pL0-9., -]{2,64}";
+	private static final Pattern ADDRESS_STRING_PATTERN = Pattern.compile(ADDRESS_STRING_REGEXP);
 	private String type;
 
 	private ProviderField(String type) {
 		this.type = type;
 	}
 
-	public boolean validate(String value) {
+	public String validPattern() {
 		switch (this) {
 			case ADDRESS: {
-				return false;
+				return "Адрес может содержать любые символы кириллицы, цифры, пробелы, запятые и точки";
 			}
 			case NAME: {
-				return false;
+				return "Имя может содержать любые символы кириллицы, и один дефис";
 			}
 			case PHONE: {
-				return false;
+				return "Номер должен начинаться с 375, далее две цифры - код оператора, далее - семизначный номер";
+			}
+			default: {
+				return "";
+			}
+		}
+	}
+
+	public boolean validate(String value) {
+		if (value == null || value.isEmpty() || value.trim().isEmpty()) {
+			return false;
+		}
+		switch (this) {
+			case ADDRESS: {
+				if (value.startsWith("/s")) {
+					return false;
+				} else if (value.replaceAll("/s", "").isEmpty()) {
+					return false;
+				}
+				return ADDRESS_STRING_PATTERN.matcher(value).matches();
+			}
+			case NAME: {
+				if (value.startsWith("/s")) {
+					return false;
+				} else if (value.replaceAll("/s", "").isEmpty()) {
+					return false;
+				}
+				return NAME_STRING_PATTERN.matcher(value).matches();
+			}
+			case PHONE: {
+				return PHONE_STRING_PATTERN.matcher(value).matches();
 			}
 			default: {
 				return false;
---
+++ ibs/ibs-common/src/test/java/edu/ibs/common/enums/ProviderFieldTest.java
@@ -0,0 +1,82 @@
+package edu.ibs.common.enums;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vadim
+ */
+public class ProviderFieldTest {
+
+	public ProviderFieldTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testPhoneValidate() {
+		ProviderField phone = ProviderField.PHONE;
+		assertTrue(phone.validate("375447655483"));
+		assertTrue(phone.validate("375224655483"));
+		assertFalse(phone.validate("275224655483"));
+		assertFalse(phone.validate("325224655483"));
+		assertFalse(phone.validate("372224655483"));
+		assertFalse(phone.validate("37522465548a"));
+		assertFalse(phone.validate(null));
+		assertFalse(phone.validate(" 375224655483"));
+		assertFalse(phone.validate("375224655483 "));
+		assertFalse(phone.validate(" 37522465548"));
+		assertFalse(phone.validate("37522465548 "));
+		assertFalse(phone.validate("a37522465548"));
+		assertFalse(phone.validate(""));
+		assertFalse(phone.validate(" "));
+		assertFalse(phone.validate("            "));
+	}
+
+	@Test
+	public void testNameValidate() {
+		ProviderField name = ProviderField.NAME;
+		assertFalse(name.validate("Vadim"));
+		assertTrue(name.validate("Вадим"));
+		assertTrue(name.validate("Мартос"));
+		assertFalse(name.validate("Вадим Мартос"));
+		assertTrue(name.validate("Вадим-Мартос"));
+		assertFalse(name.validate(" Вадим-Мартос"));
+		assertFalse(name.validate("В-адим-Мартос"));
+		assertFalse(name.validate("     "));
+		assertFalse(name.validate(""));
+		assertFalse(name.validate(null));
+		assertFalse(name.validate("-вадим"));
+		assertFalse(name.validate("!вадим"));
+		assertFalse(name.validate("-------"));
+		assertFalse(name.validate("Vaдим"));
+	}
+
+	public void testAddressValidate() {
+		ProviderField address = ProviderField.ADDRESS;
+		assertTrue(address.validate("Барановичи"));
+		assertTrue(address.validate("Барановичи, Беларусь"));
+		assertTrue(address.validate("Барановичи, Беларусь, ул. Глинки"));
+		assertTrue(address.validate("Барановичи, Беларусь, ул. Глинки, д. 13а"));
+		assertFalse(address.validate(""));
+		assertFalse(address.validate("  "));
+		assertFalse(address.validate("..."));
+		assertFalse(address.validate(".Барановичи"));
+		assertFalse(address.validate(null));
+	}
+}
