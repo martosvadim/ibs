eba6db3790cc33de00a7ed4e77ae01122a2cf9cb
Vadim Martos <vadim.martos@gmail.com>
2013-01-13 00:42:27 +0300

    added user names validation

--- ibs/ibs-core/src/main/java/edu/ibs/core/entity/User.java
+++ ibs/ibs-core/src/main/java/edu/ibs/core/entity/User.java
@@ -1,6 +1,7 @@
 package edu.ibs.core.entity;
 
 import edu.ibs.common.dto.UserDTO;
+import edu.ibs.common.enums.ProviderField;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -17,184 +18,202 @@ import java.util.regex.Pattern;
 @XmlRootElement
 public class User implements Serializable, AbstractEntity {
 
-    public static final String PASSPORT_NUMBER_REGEXP = "^[A-Z]{2}[0-9]{7}$";
-    private static final Pattern PASSPORT_NUMBER_PATTERN = Pattern.compile(PASSPORT_NUMBER_REGEXP);
-    private static final long serialVersionUID = 432949376663998234L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "id", unique = true, updatable = false)
-    private long id;
-    @Basic(optional = false)
-    @Column(name = "firstName")
-    private String firstName;
-    @Basic(optional = false)
-    @Column(name = "lastName")
-    private String lastName;
-    @Basic(optional = false)
-    @Column(name = "passportNumber")
-    private String passportNumber;
-    @Basic(optional = true)
-    @Lob
-    @Column(name = "passportScan", nullable = true)
-    private byte[] passportScan;
-    @Basic(optional = false)
-    @Column(name = "freezed")
-    private boolean freezed;
-    @Basic(optional = true)
-    @Column(name = "address")
-    private String address;
-    @Basic(optional = true)
-    @Column(name = "zipCode")
-    private Integer zipCode;
-    @Basic(optional = true)
-    @Column(name = "phone1")
-    private BigInteger phone1;
-    @Basic(optional = true)
-    @Column(name = "phone2")
-    private BigInteger phone2;
-    @Basic(optional = true)
-    @Column(name = "phone3")
-    private BigInteger phone3;
-    @Basic(optional = true)
-    @Column(name = "description")
-    private String description;
+	public static final String PASSPORT_NUMBER_REGEXP = "^[A-Z]{2}[0-9]{7}$";
+	private static final Pattern PASSPORT_NUMBER_PATTERN = Pattern.compile(PASSPORT_NUMBER_REGEXP);
+	private static final long serialVersionUID = 432949376663998234L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "id", unique = true, updatable = false)
+	private long id;
+	@Basic(optional = false)
+	@Column(name = "firstName")
+	private String firstName;
+	@Basic(optional = false)
+	@Column(name = "lastName")
+	private String lastName;
+	@Basic(optional = false)
+	@Column(name = "passportNumber")
+	private String passportNumber;
+	@Basic(optional = true)
+	@Lob
+	@Column(name = "passportScan", nullable = true)
+	private byte[] passportScan;
+	@Basic(optional = false)
+	@Column(name = "freezed")
+	private boolean freezed;
+	@Basic(optional = true)
+	@Column(name = "address")
+	private String address;
+	@Basic(optional = true)
+	@Column(name = "zipCode")
+	private Integer zipCode;
+	@Basic(optional = true)
+	@Column(name = "phone1")
+	private BigInteger phone1;
+	@Basic(optional = true)
+	@Column(name = "phone2")
+	private BigInteger phone2;
+	@Basic(optional = true)
+	@Column(name = "phone3")
+	private BigInteger phone3;
+	@Basic(optional = true)
+	@Column(name = "description")
+	private String description;
 
-    public User() {
-    }
+	public User() {
+	}
 
-    public User(String firstName, String lastName, String passportNumber) throws IllegalArgumentException {
-        if (!User.validatePassportNumber(passportNumber)) {
-            throw new IllegalArgumentException(String.format("Invalid passport number %s, must be: two latin letters in upper case and 7 numbers without any spaces", passportNumber));
-        }
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.passportNumber = passportNumber;
-    }
+	public User(String firstName, String lastName, String passportNumber) throws IllegalArgumentException {
+		if (!User.validatePassportNumber(passportNumber)) {
+			throw new IllegalArgumentException(String.format("Invalid passport number %s, must be: two latin letters in upper case and 7 numbers without any spaces", passportNumber));
+		}
+		if (!User.validateName(firstName)) {
+			throw new IllegalArgumentException(String.format("Invalid first name %s, can contains cyrillic symbols separated by '-' once only", firstName));
+		}
+		if (!User.validateName(lastName)) {
+			throw new IllegalArgumentException(String.format("Invalid last name %s, can contains cyrillic symbols separated by '-' once only", lastName));
+		}
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.passportNumber = passportNumber;
+	}
 
-    public User(UserDTO dto) {
-        this(dto.getFirstName(), dto.getLastName(), dto.getPassportNumber());
-        setAddress(dto.getAddress());
-        setDescription(dto.getDescription());
-        setPhone1(dto.getPhone1());
-        setPhone2(dto.getPhone2());
-        setPhone3(dto.getPhone3());
-        setPassportScan(dto.getPassportScan());
-        setZipCode(dto.getZipCode());
-        this.id = dto.getId();
-    }
+	public User(UserDTO dto) {
+		this(dto.getFirstName(), dto.getLastName(), dto.getPassportNumber());
+		setAddress(dto.getAddress());
+		setDescription(dto.getDescription());
+		setPhone1(dto.getPhone1());
+		setPhone2(dto.getPhone2());
+		setPhone3(dto.getPhone3());
+		setPassportScan(dto.getPassportScan());
+		setZipCode(dto.getZipCode());
+		this.id = dto.getId();
+	}
 
-    public static boolean validatePassportNumber(String passportNumber) {
-        if (passportNumber == null) {
-            return false;
-        }
-        return PASSPORT_NUMBER_PATTERN.matcher(passportNumber).matches();
-    }
+	public static boolean validatePassportNumber(String passportNumber) {
+		if (passportNumber == null) {
+			return false;
+		}
+		return PASSPORT_NUMBER_PATTERN.matcher(passportNumber).matches();
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public static boolean validateAddress(String address) {
+		return ProviderField.ADDRESS.validate(address);
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public static boolean validateName(String name) {
+		return ProviderField.NAME.validate(name);
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public static boolean validatePhone(String phone) {
+		return ProviderField.PHONE.validate(phone);
+	}
 
-    public boolean isFreezed() {
-        return freezed;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getPassportNumber() {
-        return passportNumber;
-    }
+	public boolean isFreezed() {
+		return freezed;
+	}
 
-    public byte[] getPassportScan() {
-        return passportScan;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public BigInteger getPhone1() {
-        return phone1;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public BigInteger getPhone2() {
-        return phone2;
-    }
+	public String getPassportNumber() {
+		return passportNumber;
+	}
 
-    public BigInteger getPhone3() {
-        return phone3;
-    }
+	public byte[] getPassportScan() {
+		return passportScan;
+	}
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
+	public BigInteger getPhone1() {
+		return phone1;
+	}
 
-    public Integer getZipCode() {
-        return zipCode;
-    }
+	public BigInteger getPhone2() {
+		return phone2;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public BigInteger getPhone3() {
+		return phone3;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
 
-    public void setPassportScan(byte[] passportScan) {
-        this.passportScan = passportScan;
-    }
+	public Integer getZipCode() {
+		return zipCode;
+	}
 
-    public void setPhone1(BigInteger phone1) {
-        this.phone1 = phone1;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public void setPhone2(BigInteger phone2) {
-        this.phone2 = phone2;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setPhone3(BigInteger phone3) {
-        this.phone3 = phone3;
-    }
+	public void setPassportScan(byte[] passportScan) {
+		this.passportScan = passportScan;
+	}
 
-    public void setZipCode(Integer zipCode) {
-        this.zipCode = zipCode;
-    }
+	public void setPhone1(BigInteger phone1) {
+		this.phone1 = phone1;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        return true;
-    }
+	public void setPhone2(BigInteger phone2) {
+		this.phone2 = phone2;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 61 * hash + (int) (this.id ^ (this.id >>> 32));
-        return hash;
-    }
+	public void setPhone3(BigInteger phone3) {
+		this.phone3 = phone3;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" + "firstName=" + firstName + ", lastName=" + lastName + '}';
-    }
+	public void setZipCode(Integer zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final User other = (User) obj;
+		if (this.id != other.id) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 61 * hash + (int) (this.id ^ (this.id >>> 32));
+		return hash;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" + "firstName=" + firstName + ", lastName=" + lastName + '}';
+	}
 }
