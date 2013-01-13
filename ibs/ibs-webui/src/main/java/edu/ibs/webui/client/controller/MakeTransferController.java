9cb4213a0401d867ff6971392ddb1074612ef578
Vadim Martos <vadim.martos@gmail.com>
2013-01-13 22:00:19 +0300

    made description more readable

--- ibs/ibs-webui/src/main/java/edu/ibs/webui/client/controller/MakeTransferController.java
+++ ibs/ibs-webui/src/main/java/edu/ibs/webui/client/controller/MakeTransferController.java
@@ -23,80 +23,79 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * User: Максим
- * Date: 11.01.13
- * Time: 2:48
+ * User: Максим Date: 11.01.13 Time: 2:48
  */
 public class MakeTransferController extends GenericWindowController {
 
-    private IAction<Object> closeAction = null;
-
+	private IAction<Object> closeAction = null;
 	private final GenericController amount = Components.getTextItem();
-    private GenericController reciepient = Components.getComboBoxControll();
-
+	private GenericController reciepient = Components.getComboBoxControll();
 	private CardBookDTO cardBookDTO;
-    private List<ProviderDTO> contragents = new ArrayList<ProviderDTO>();
-    private VLayout providerFieldsLayout = new VLayout();
-    private List<Canvas> providerFieldViews = new ArrayList<Canvas>();
-    private Map<ProviderField, GenericController> fieldsMap = new HashMap<ProviderField, GenericController>();
+	private List<ProviderDTO> contragents = new ArrayList<ProviderDTO>();
+	private VLayout providerFieldsLayout = new VLayout();
+	private List<Canvas> providerFieldViews = new ArrayList<Canvas>();
+	private Map<ProviderField, GenericController> fieldsMap = new HashMap<ProviderField, GenericController>();
 
 	public MakeTransferController() {
 		getWindow().setTitle("Оплата");
 		final IButton payButton = new IButton("Оплатить");
 		payButton.setWidth(80);
 
-        IPaymentServiceAsync.Util.getInstance().getContragentList(new AppCallback<List<ProviderDTO>>() {
-            @Override
-            public void onSuccess(List<ProviderDTO> providerDTOs) {
-                if (providerDTOs != null && providerDTOs.size() > 0) {
-                    contragents = providerDTOs;
-                    List<VocDTO<String, String>> bindList = new LinkedList<VocDTO<String, String>>();
-                    for (ProviderDTO dto : providerDTOs) {
-                        VocDTO<String, String> vocDTO = new VocDTO<String, String>(
-                                    String.valueOf(dto.getId()), String.valueOf(dto.getCard().getBankBook().getDescription()));
-                        bindList.add(vocDTO);
-                    }
-                    reciepient.bind(bindList);
-                }
-            }
-        });
+		IPaymentServiceAsync.Util.getInstance().getContragentList(new AppCallback<List<ProviderDTO>>() {
 
-        reciepient.addOnChange(new IAction() {
-            @Override
-            public void execute(Object data) {
-                providerFieldsLayout.setVisible(true);
-                for (Canvas c : providerFieldViews) {
-                    providerFieldsLayout.removeMember(c);
-                }
-                providerFieldViews.clear();
-                fieldsMap.clear();
-                String id = ((VocDTO<String, String>) reciepient.unbind()).getId();
-                ProviderDTO dto = null;
-                if (id != null && id.length() > 0) {
-                    for (ProviderDTO pdto : contragents) {
-                        if (pdto.getId() == Long.parseLong(id)) {
-                            dto = pdto;
-                            break;
-                        }
-                    }
-                    for (ProviderField field : dto.getFieldList()) {
-                        GenericController controller = Components.getTextItem();
-                        Canvas providerFieldView = Components.addTitle(getProviderFieldTitle(field), controller.getView());
-                        providerFieldsLayout.addMember(providerFieldView);
-                        fieldsMap.put(field, controller);
-                        providerFieldViews.add(providerFieldView);
-                    }
+			@Override
+			public void onSuccess(List<ProviderDTO> providerDTOs) {
+				if (providerDTOs != null && providerDTOs.size() > 0) {
+					contragents = providerDTOs;
+					List<VocDTO<String, String>> bindList = new LinkedList<VocDTO<String, String>>();
+					for (ProviderDTO dto : providerDTOs) {
+						VocDTO<String, String> vocDTO = new VocDTO<String, String>(
+								String.valueOf(dto.getId()), String.valueOf(dto.getCard().getBankBook().getDescription()));
+						bindList.add(vocDTO);
+					}
+					reciepient.bind(bindList);
+				}
+			}
+		});
 
-                    providerFieldsLayout.redraw();
-                }
+		reciepient.addOnChange(new IAction() {
 
-            }
-        });
+			@Override
+			public void execute(Object data) {
+				providerFieldsLayout.setVisible(true);
+				for (Canvas c : providerFieldViews) {
+					providerFieldsLayout.removeMember(c);
+				}
+				providerFieldViews.clear();
+				fieldsMap.clear();
+				String id = ((VocDTO<String, String>) reciepient.unbind()).getId();
+				ProviderDTO dto = null;
+				if (id != null && id.length() > 0) {
+					for (ProviderDTO pdto : contragents) {
+						if (pdto.getId() == Long.parseLong(id)) {
+							dto = pdto;
+							break;
+						}
+					}
+					for (ProviderField field : dto.getFieldList()) {
+						GenericController controller = Components.getTextItem();
+						Canvas providerFieldView = Components.addTitle(getProviderFieldTitle(field), controller.getView());
+						providerFieldsLayout.addMember(providerFieldView);
+						fieldsMap.put(field, controller);
+						providerFieldViews.add(providerFieldView);
+					}
+
+					providerFieldsLayout.redraw();
+				}
+
+			}
+		});
 
 		payButton.addClickHandler(new com.smartgwt.client.widgets.events.ClickHandler() {
+
 			@Override
 			public void onClick(com.smartgwt.client.widgets.events.ClickEvent clickEvent) {
-                VocDTO<String, String> recVoc = ((VocDTO<String, String>) reciepient.unbind());
+				VocDTO<String, String> recVoc = ((VocDTO<String, String>) reciepient.unbind());
 				final String reciepientId = recVoc.getId();
 				final String amountTxt = ((String) amount.unbind());
 				if (reciepientId == null || "".equals(reciepientId) || reciepientId.length() == 0) {
@@ -109,33 +108,34 @@ public class MakeTransferController extends GenericWindowController {
 						if (amountFloat <= 0f) {
 							SC.warn("Введите положительное значение.");
 						} else {
-                            if (validateFields()) {
-                                payButton.setDisabled(true);
+							if (validateFields()) {
+								payButton.setDisabled(true);
 
-                                String description = "";
-                                for (ProviderField field : fieldsMap.keySet()) {
-                                    description += fieldsMap.get(field).unbind() + ";";
-                                }
+								String description = "";
+								for (ProviderField field : fieldsMap.keySet()) {
+									description += String.format("%s: %s; ", field.toString(), fieldsMap.get(field).unbind());;
+								}
 
-                                IPaymentServiceAsync.Util.getInstance().pay(getCardBookDTO(), reciepientId, amountFloat,
-                                        TransactionType.PAYMENT, description, new AppCallback<Void>() {
-                                            @Override
-                                            public void onFailure(Throwable t) {
-                                                super.onFailure(t);
-                                                payButton.setDisabled(false);
-                                            }
+								IPaymentServiceAsync.Util.getInstance().pay(getCardBookDTO(), reciepientId, amountFloat,
+										TransactionType.PAYMENT, description, new AppCallback<Void>() {
 
-                                            @Override
-                                            public void onSuccess(Void aVoid) {
-                                                payButton.setDisabled(false);
-                                                SC.say("Платёж успешно проведён");
-                                                if (closeAction != null) {
-                                                    closeAction.execute(null);
-                                                }
-                                                getWindow().hide();
-                                            }
-                                        });
-                            }
+									@Override
+									public void onFailure(Throwable t) {
+										super.onFailure(t);
+										payButton.setDisabled(false);
+									}
+
+									@Override
+									public void onSuccess(Void aVoid) {
+										payButton.setDisabled(false);
+										SC.say("Платёж успешно проведён");
+										if (closeAction != null) {
+											closeAction.execute(null);
+										}
+										getWindow().hide();
+									}
+								});
+							}
 						}
 					} catch (NumberFormatException e) {
 						SC.warn("Введите корректную сумму.");
@@ -148,11 +148,11 @@ public class MakeTransferController extends GenericWindowController {
 		layoutForm.setHeight100();
 
 		layoutForm.addMember(Components.addTitle("Получатель", reciepient.getView()));
-        layoutForm.addMember(providerFieldsLayout);
-        providerFieldsLayout.setVisible(false);
-        VLayout amountLayout = new VLayout();
-        amountLayout.setHeight100();
-        amountLayout.addMember(Components.addTitle("Сумма", amount.getView()));
+		layoutForm.addMember(providerFieldsLayout);
+		providerFieldsLayout.setVisible(false);
+		VLayout amountLayout = new VLayout();
+		amountLayout.setHeight100();
+		amountLayout.addMember(Components.addTitle("Сумма", amount.getView()));
 		layoutForm.addMember(amountLayout);
 
 		HLayout buttons = new HLayout();
@@ -168,33 +168,33 @@ public class MakeTransferController extends GenericWindowController {
 		getWindow().addItem(view);
 	}
 
-    private String getProviderFieldTitle(ProviderField field) {
-        switch (field) {
-            case BOOK:
-                return "Счёт";
-            case PHONE:
-                return "№ телефона";
-            case ADDRESS:
-                return "Адрес";
-            case NAME:
-                return "Имя";
-            case PASSPORT:
-                return "№ пасспорта";
-            default:
-                return "";
-        }
-    }
+	private String getProviderFieldTitle(ProviderField field) {
+		switch (field) {
+			case BOOK:
+				return "Счёт";
+			case PHONE:
+				return "№ телефона";
+			case ADDRESS:
+				return "Адрес";
+			case NAME:
+				return "Имя";
+			case PASSPORT:
+				return "№ пасспорта";
+			default:
+				return "";
+		}
+	}
 
-    private boolean validateFields() {
-        for (ProviderField field : fieldsMap.keySet()) {
-            GenericController controller = fieldsMap.get(field);
-            if (!field.validate((String) controller.unbind())) {
-                SC.say(field.validPattern());
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean validateFields() {
+		for (ProviderField field : fieldsMap.keySet()) {
+			GenericController controller = fieldsMap.get(field);
+			if (!field.validate((String) controller.unbind())) {
+				SC.say(field.validPattern());
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public CardBookDTO getCardBookDTO() {
 		return cardBookDTO;
@@ -202,16 +202,16 @@ public class MakeTransferController extends GenericWindowController {
 
 	public void setCardBookDTO(CardBookDTO cardBookDTO) {
 		this.cardBookDTO = cardBookDTO;
-        DynamicForm form = ((DynamicForm)amount.getView());
-        ((TextItem) form.getFields()[0]).setShowHintInField(true);
-        form.getFields()[0].setHint(Components.getCurrencyHint(cardBookDTO.getBankBook().getCurrency()));
+		DynamicForm form = ((DynamicForm) amount.getView());
+		((TextItem) form.getFields()[0]).setShowHintInField(true);
+		form.getFields()[0].setHint(Components.getCurrencyHint(cardBookDTO.getBankBook().getCurrency()));
 	}
 
-    public IAction<Object> getCloseAction() {
-        return closeAction;
-    }
+	public IAction<Object> getCloseAction() {
+		return closeAction;
+	}
 
-    public void setCloseAction(IAction<Object> closeAction) {
-        this.closeAction = closeAction;
-    }
+	public void setCloseAction(IAction<Object> closeAction) {
+		this.closeAction = closeAction;
+	}
 }
